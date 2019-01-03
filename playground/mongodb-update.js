a3528a70b0275aa76ecba2bac9a006343df6f718
AlexSmirnov <alks.smirnov@gmail.com>
2019-01-02 20:24:17 -0500

    Added update DB file

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('5c23e6d0f9c5ad84f0b8eaa3')
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    // Find user Jen and change the name to Oleksii, increase
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('5c23e8f6420a4500f9787e06')
+    }, {
+        $set: {
+            name: 'Oleksii'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+    // db.close();
+});
