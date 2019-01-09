0dc93dff923b98262275560063b38322b7b114a8
Alex Smirnov <alks.smirnov@gmail.com>
2019-01-09 17:55:37 -0500

    added Mongoose queries and POST /users route

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,60 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+
+const userId = "5c3679a9250744fc76eec80e";
+
+// const id = '5c3671f433439a7072f89e6a';
+//
+// if (!ObjectID.isValid(id)) {
+//     console.log('ID not valid');
+// }
+
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Todos', JSON.stringify(todos, undefined, 2));
+// });
+//
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log('Todo', JSON.stringify(todo, undefined, 2));
+// });
+
+// Todo.findById(id).then((todo) => {
+//     if (!todo) {
+//         return console.log('Id not found');
+//     }
+//     console.log('Todo using findById', JSON.stringify(todo, undefined, 2));
+// }).catch((e) => console.log(e));
+
+User.findById(userId).then((user) => {
+    if (!user) {
+        return console.log('User was not found');
+    }
+    console.log('User found: ' + JSON.stringify(user, undefined, 2));
+}).catch((e) => {
+    console.log(e);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
