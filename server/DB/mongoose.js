cf217cbdd86b4d0485e6324f3bc9185d98cadd59
Alex Smirnov <alks.smirnov@gmail.com>
2019-01-29 14:41:42 -0500

    Refactoring

--- server/DB/mongoose.js
+++ server/DB/mongoose.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
@@ -16,4 +16,3 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
 module.exports = {
     mongoose
 };
->>>>>>> 7a3d67d57e43cfce55f9891431bad3e2800a2a8a
