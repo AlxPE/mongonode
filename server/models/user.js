2ff505996858ee5ce547dd7a78d4ae12f022c226
Alex Smirnov <alks.smirnov@gmail.com>
2019-08-07 18:10:36 -0400

    added DELETE users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -90,6 +90,17 @@ UserSchema.statics.findByCredentials = function (email, password) {
     });
 };
 
+
+UserSchema.methods.removeToken = function (token) {
+    var user = this;
+
+    return user.update({
+        $pull: {
+            tokens: {token}
+        }
+    })
+};
+
 UserSchema.pre('save', function(next){
     var user = this;
 
@@ -106,6 +117,7 @@ UserSchema.pre('save', function(next){
     }
 
 });
+
 
 const User = mongoose.model('User', UserSchema);
 
